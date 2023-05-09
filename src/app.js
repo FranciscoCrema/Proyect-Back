7620db5ebd98f18e79d1e49f132cd4f8c6d8febc
Francisco Crema <franciscocrema@MacBook-Air-de-Francisco.local>
2023-05-09 18:49:15 -0300

    metiendo busqueda de productos y tambien por id

--- src/app.js
+++ src/app.js
@@ -5,27 +5,27 @@ const port = 8080;
 
 const product = new ProductManager();
 const readProduct = product.readProducts();
-console.log(readProduct);
-// product.addProducts("Titel1", "Description1", 200, "img1", "56207062", 25);
-// product.addProducts("Titel2", "Description2", 500, "img2", "56189368", 50);
-// product.addProducts("Titel3", "Description3", 380, "img3", "51763580", 75);
-// product.addProducts("Titel4", "Description4", 100, "img4", "75037140", 30);
-// product.addProducts("Titel5", "Description5", 550, "img5", "32750236", 80);
-// product.addProducts("Titel6", "Description6", 480, "img6", "01734875", 15);
-// product.addProducts("Titel7", "Description7", 800, "img7", "65083175", 100);
-// product.addProducts("Titel8", "Description8", 50, "img8", "17659309", 38);
-// product.addProducts("Titel9", "Description9", 80, "img9", "65794658", 90);
-// product.addProducts("Titel10", "Description10", 680, "img10", "16564082", 45);
 
-app.get("/", (req, res) => {
-  res.send("Holaaaa");
+/* Muestro todos los productos o mediante query el producto que uno guste */
+app.get("/products", (req, res) => {
+  const limit = req.query.limit;
+  if (!limit) {
+    res.send(readProduct);
+  } else {
+    res.send(readProduct.slice(0, limit));
+  }
 });
 
-app.get("/fran", (req, res) => {
-  res.json({
-    nombre: "fran",
-    apellido: "crema",
-  });
+/* Mostrando producto por id */
+
+app.get("/products/:id", (req, res) => {
+  let id = req.params.id;
+  const productId = product.getProductsById(parseInt(id));
+  if (productId) {
+    res.send(productId);
+  } else {
+    console.log("No existe el id");
+  }
 });
 
 app.listen(port, () => {
