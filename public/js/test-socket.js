e1208c95cfa49d004820dea3af097a5ace04fa49
Francisco Crema <franciscocrema@MacBook-Air-de-Francisco.local>
2023-06-14 01:17:55 -0300

    agregando wetsocket

---
+++ public/js/test-socket.js
@@ -0,0 +1,12 @@
+const socket = io();
+
+setInterval(() => {
+  socket.emit("msg_front_back", {
+    msg: "hola front",
+    user: "usuario",
+  });
+}, 1000);
+
+socket.on("msg_back_front", (msg) => {
+  console.log(msg);
+});
--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import express from "express";
 import prod from "./routes/products.router.js";
 import cartRouter from "./routes/cart.router.js";
 import { testPlantillaProducts } from "./routes/test-plantilla-products.router.js";
+import { testSocketRouter } from "./routes/test-socket.router.js";
 import handlebars from "express-handlebars";
 import { __dirname } from "./utils.js";
+import { Server } from "socket.io";
 
 const app = express();
 const port = 8080;
@@ -14,12 +16,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", prod);
 app.use("/api/carts", cartRouter);
 
+app.use(express.static((__dirname, "public")));
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
 
 app.use("/test-plantilla-products", testPlantillaProducts);
 
-app.listen(port, () => {
+app.use("/test-socket", testSocketRouter);
+
+const httpServer = app.listen(port, () => {
   console.log(`Se esta viendo en http://localhost:${port}`);
+});
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", (socket) => {
+  setInterval(() => {
+    socket.emit("msg_back_front", {
+      msg: "hola back",
+      user: "usuario",
+    });
+  }, 1000);
+
+  socket.on("msg_front_back", (msg) => {
+    console.log(msg);
+  });
 });
